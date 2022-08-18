2508cbdac69ad1246259631d626a41d94efdcb0c
cempod <cempod@gmail.com>
2022-08-18 14:51:40 +0300

    test

--- src/modules/cempod_module/cempod_module.cpp
+++ src/modules/cempod_module/cempod_module.cpp
@@ -35,10 +35,17 @@
 
 #include <px4_platform_common/getopt.h>
 #include <px4_platform_common/log.h>
+#include <px4_platform_common/tasks.h>
 #include <px4_platform_common/posix.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <poll.h>
+#include <string.h>
+#include <math.h>
 
 #include <uORB/uORB.h>
-#include <uORB/topics/vehicle_acceleration.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/sensor_accel.h>
 #include <uORB/topics/vehicle_attitude.h>
 
 
@@ -106,15 +113,51 @@ CempodModule::CempodModule(int example_param)
 void CempodModule::run()
 {
 PX4_INFO("Hello NII STT");
+
+/* subscribe to sensor_combined topic */
+	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_accel));
+	/* limit the update rate to 5 Hz */
+	orb_set_interval(sensor_sub_fd, 200);
+	px4_pollfd_struct_t fds[] = {
+		{ .fd = sensor_sub_fd,   .events = POLLIN },
+		/* there could be more file descriptors here, in the form like:
+		 * { .fd = other_sub_fd,   .events = POLLIN },
+		 */
+	};
+	int error_counter = 0;
 	while (!should_exit()) {
+int poll_ret = px4_poll(fds, 1, 1000);
+if (poll_ret == 0) {
+			/* this means none of our providers is giving us data */
+			PX4_ERR("Got no data within a second");
+
+		} else if (poll_ret < 0) {
+			/* this is seriously bad - should be an emergency */
+			if (error_counter < 10 || error_counter % 50 == 0) {
+				/* use a counter to prevent flooding (and slowing us down) */
+				PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+			}
+
+			error_counter++;
+
+		} else {
+if (fds[0].revents & POLLIN) {
+				/* obtained data for the first file descriptor */
+				struct sensor_combined_s raw;
+				/* copy sensors raw data into local buffer */
+				orb_copy(ORB_ID(sensor_accel), sensor_sub_fd, &raw);
+				PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
+					 (double)raw.accelerometer_m_s2[0],
+					 (double)raw.accelerometer_m_s2[1],
+					 (double)raw.accelerometer_m_s2[2]);
 
 
-
-			px4_usleep(100000);
+			px4_usleep(1000000);
 	}
 
 }
-
+}
+}
 
 
 int CempodModule::print_usage(const char *reason)
